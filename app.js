b2886d8da70b7e72a884d3ab6eef9320057322f3
EC2 Default User <ec2-user@ip-172-31-14-73.us-west-1.compute.internal>
2020-11-24 02:31:48 +0000

    Lab4 - Final

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ app.use(express.static("public"));
 
 //routes
 app.get("/", function(req, res) {
-    res.render("index.html");
+    res.render("index.html", {active: 'home'});
 });
 
 app.get("/mercury", function(req, res) {
-     res.render("mercury.html");
+     res.render("mercury.html", {active: 'mercury'});
 });
 
 app.get("/venus", function(req, res) {
-     res.render("venus.html");
+     res.render("venus.html", {active: 'venus'});
 });
 
+app.get("/earth", function(req, res) {
+     res.render("earth.html", {active: 'earth'});
+});
+
+app.get("/jupiter", function(req, res) {
+     res.render("jupiter.html", {active: 'jupiter'});
+});
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function() {
